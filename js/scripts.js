2b579a50de59a442076b68b2cad6a2e850a68225
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-10 22:14:30 -0700

    WIP: jquery click work

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".content").click(function() {
     $(".card-showing").toggle();
     $(".card-hidden").toggle();
   });
 });
+
+
+// $(document).ready(function() {
+//   $(".content").click(function() {
+//     $(".card-showing").next().show();
+//   });
+// });
+
+
+// $('.card-showing', '.card-hidden').on('click', function() {
+//   $(this).hide();
+//   $(this).siblings('.card-showing').show();
+// });
+
+// $(document).ready(function() {
+// $( ".content" ).click(function() {
+//   $(".card-showing").next().toggle;
+//   });
+// });
